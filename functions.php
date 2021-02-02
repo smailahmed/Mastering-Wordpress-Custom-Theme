5dedee2ecde3540a61c5ceee467a5adfb95fcf9e
smailahmed <ahmed.smail252@gmail.com>
2021-02-03 00:21:15 +0100

    Add Action and Enqueue Scripts

--- functions.php
+++ functions.php
@@ -1,16 +1,19 @@
 <?php
   // Function To Add My Custom Styles
   function custom_styles () {
-    wp_enqueue_style('bootstrap-css', get_template_directory_url() 
+    wp_enqueue_style('bootstrap-css', get_template_directory_uri() 
     . '/css/bootstrap.min.css');
   }
   // Function To Add My Custom Scripts
   function custom_scripts () {
-    wp_equeue_script('bootstrap-js', get_template_directory_url()
+    wp_enqueue_script('bootstrap-js', get_template_directory_uri()
     .'js/bootstrap.bundle.min.js', array(), false, true);
-    wp_equeue_script('main-js', get_template_directory_url()
+    wp_enqueue_script('main-js', get_template_directory_uri()
     . 'js/main.js', array(), false, true);
-    wp_equeue_script('fontawesome-js', get_template_directory_url()
+    wp_enqueue_script('fontawesome-js', get_template_directory_uri()
     . 'https://kit.fontawesome.com/256e25e1a8.js', array(), false, true);
   }
+  // Add Actions
+  add_action('wp_enqueue_scripts', 'custom_styles');
+  add_action('wp_enqueue_scripts', 'custom_scripts');
 ?>
