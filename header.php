d1db3258eb00a326ae5d20e0e71d6eed4e8c3078
smailahmed <ahmed.smail252@gmail.com>
2021-02-02 21:05:20 +0100

    Structure Of WordPress Theme

---
+++ header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+  <head>
+    <meta charset="<?php bloginfo('charset'); ?>" />
+    <title><?php bloginfo('name'); ?></title>
+    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+    <?php wp_head(); ?>
+  </head>
+</html>
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php get_header(); ?>
+This Is Test Content
+<?php bloginfo('admin_email'); ?>
+<?php get_sidebar(); ?>
+<?php get_search_form(); ?>
+<?php get_footer(); ?>
