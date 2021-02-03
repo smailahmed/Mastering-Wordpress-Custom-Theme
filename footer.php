a7768787b8ba289054c158ec0efadbbe806d1f39
smailahmed <ahmed.smail252@gmail.com>
2021-02-03 14:39:35 +0100

    Enqueue && Register Script, Enqueue Scripts From Wordpress Core, Footer, Main Style CSS, Add Data To Script

---
+++ footer.php
@@ -0,0 +1,6 @@
+    <div class="footer text-center">
+      copyright &copy; 2021 GHEXE
+    </div>
+    <?php wp_footer(); ?>
+  </body>
+</html>
--- functions.php
+++ functions.php
@@ -3,15 +3,29 @@
   function custom_styles () {
     wp_enqueue_style('bootstrap-css', get_template_directory_uri() 
     . '/css/bootstrap.min.css');
+    wp_enqueue_style('main', get_template_directory_uri() 
+    . '/css/main.css');
   }
   // Function To Add My Custom Scripts
   function custom_scripts () {
+    // Remove Registeration For Old jQuery
+    wp_deregister_script('jquery');
+    // Register A New jQuery In Footer
+    wp_register_script('jquery', includes_url('/js/jquery/jquery.js'), false, '', true);
+    // Enqueue The New jQuery
+    wp_enqueue_script('jquery');
     wp_enqueue_script('bootstrap-js', get_template_directory_uri()
-    .'js/bootstrap.bundle.min.js', array(), false, true);
+    .'/js/bootstrap.bundle.min.js', array(), false, true);
     wp_enqueue_script('main-js', get_template_directory_uri()
-    . 'js/main.js', array(), false, true);
-    wp_enqueue_script('fontawesome-js', get_template_directory_uri()
-    . 'https://kit.fontawesome.com/256e25e1a8.js', array(), false, true);
+    . '/js/main.js', array(), false, true);
+    wp_enqueue_script('fontawesome-js', 'https://kit.fontawesome.com/256e25e1a8.js'
+    ,array(), false, true);
+    wp_enqueue_script('html5shiv', get_template_directory_uri()
+    . '/js/html5shiv.min.js');
+    wp_script_add_data('html5shiv', 'conditional', 'lt IE 9');
+    wp_enqueue_script('respond', get_template_directory_uri()
+    . '/js/respond.min.js');
+    wp_script_add_data('respond', 'conditional', 'lt IE 9');
   }
   // Add Actions
   add_action('wp_enqueue_scripts', 'custom_styles');
